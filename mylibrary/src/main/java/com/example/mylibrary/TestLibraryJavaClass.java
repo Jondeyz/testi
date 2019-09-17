e4673d0acf1e84f873de5ef68af590b1b33ed97e
Joona Tuomikoski <t6tujo00@students.oamk.fi>
2019-09-17 14:16:48 +0300

    testing

--- mylibrary/src/main/java/com/example/mylibrary/TestLibraryJavaClass.java
+++ mylibrary/src/main/java/com/example/mylibrary/TestLibraryJavaClass.java
@@ -5,7 +5,13 @@ import android.widget.Toast;
 
 public class TestLibraryJavaClass {
 
+    //Käytin tätä apuna: https://www.youtube.com/watch?v=obO-gxev4HA
     public void doToast(Context ctx, String str){
+        Toast.makeText(ctx, str, Toast.LENGTH_SHORT).show();
+    }
+    
+    public void shortToast(String str){
+        Context ctx = null;
         Toast.makeText(ctx, str, Toast.LENGTH_SHORT).show();
     }
 
